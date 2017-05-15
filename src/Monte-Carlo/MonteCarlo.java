e5aad08224e10306544d08c16765bcf8b6c05d57
craftsmon <craftsmon@outlook.com>
2017-05-15 09:07:05 +0300

    Working MonteCarlo

--- src/Monte-Carlo/MonteCarlo.java
+++ src/Monte-Carlo/MonteCarlo.java
@@ -3,16 +3,18 @@ import java.util.*;
 import java.io.*;
 import java.lang.Math;
 
-// use MonteCarlo integration to find an esstimate to the function
-// sin2(x) + cos2(x)
+/*
+ * Monte Carlo Integration
+ * Find the integal of squared(sinx) + squared(cosx)
+ * from 0 to PI
+ */
 class MonteCarlo
 {
    // this function uses the equation a + u(a - b)
    // to generate a uniform X value for a function parameter
    public static double generateUniformValue(double a, double b)
    {
-      Random random = new Random();
-      double randomValue = a + random.nextDouble() * (a - b);
+      double randomValue = a + Math.random() * (b - a);
       return randomValue;
    }
    public static void main(String[] args)
